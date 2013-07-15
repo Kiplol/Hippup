c7a9c95649e7cec9bc55fe36c5c817ac58717d03
Elliott Kipper <Kip@Toppington.local>
2013-07-14 17:54:40 -0700

    Layout for HUInfoCell

--- Hippup/Hippup/Views/HUInfoCell.m
+++ Hippup/Hippup/Views/HUInfoCell.m
@@ -21,6 +21,21 @@
     return self;
 }
 
+-(void)layoutSubviews
+{
+    [super layoutSubviews];
+    [_lblKey sizeToFit];
+    CGFloat maxValWidth = self.bounds.size.width - (CGRectGetMaxX(_lblKey.frame) + 10.0f);
+    [_lblValue sizeToFit];
+    if(_lblValue.bounds.size.width > maxValWidth)
+    {
+        _lblValue.frame = CGRectMake(CGRectGetMaxX(_lblKey.frame) + 5.0f, _lblValue.frame.origin.y, maxValWidth, _lblValue.frame.size.height);
+    }
+    else
+    {
+        _lblValue.frame = CGRectMake(self.bounds.size.width - 5.0f - _lblValue.bounds.size.width, _lblValue.frame.origin.y, _lblValue.bounds.size.width, _lblValue.bounds.size.height);
+    }
+}
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
